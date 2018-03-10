c326f09b72e04e1e3629b36a8776efbfce845d72
Nisaruj <nisaruj@hotmail.com>
2018-03-10 21:00:00 +0700

    Add help command

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mathjs = require('mathjs');
+var help = require('./help.json');
 
 const Client = require('@line/bot-sdk').Client;
 const middleware = require('@line/bot-sdk').middleware
@@ -31,8 +32,14 @@ app.post('/webhook',function(req, res) {
             var output = 'Nothing';
             if (getText.slice(0,4).toLowerCase() === 'calc') {
                 output = mathjs.eval(getText.slice(getText.search(' '))).toString();   
+            } else if (getText.toLowerCase() === 'help') {
+                output = ''
+                for (var i=0;i<help.list.length;i++) {
+                    output += help.list[i][0] + ' : '+ help.list[i][1] + '\n';
+                }
             } else {
-                output = "you typed : " + getText;
+                output = "you typed : " + getText + '\n';
+                output += "type <help> to get all commands";
             }
             client.pushMessage(event.source.userId,{type: "text", text: output});
             console.log(event.source.userId + ' : ' + getText);
